a11a1b7d1d99301cbe7bc38c073c498cf389d178
Dominic Hamon <dma+github@stripysock.com>
2021-10-11 21:57:01 +0100

    antialiasing

--- src/main.zig
+++ src/main.zig
@@ -7,11 +7,16 @@ const rgb = @import("rgb.zig");
 const sphere = @import("sphere.zig");
 const vec3 = @import("vec3.zig");
 
+pub const log_level: std.log.Level = .info;
+const samples = 50;
+const width = 400;
+const stdout = std.io.getStdOut().writer();
+
 fn lerp(a: vec3.Vec3, b: vec3.Vec3, t: f32) vec3.Vec3 {
     return a.mult(1.0 - t).add(b.mult(t));
 }
 
-fn ray_color(r: ray.Ray, spheres: []sphere.Sphere) rgb.RGB {
+fn ray_color(r: ray.Ray, spheres: []sphere.Sphere) vec3.Vec3 {
     var nearest: f32 = 100.0;
     var nearestHit: ?hit.Hit = null;
     for (spheres) |sph| {
@@ -22,12 +27,12 @@ fn ray_color(r: ray.Ray, spheres: []sphere.Sphere) rgb.RGB {
         }
     }
     if (nearestHit) |h| {
-        return rgb.RGB.fromVec3(h.n.add(vec3.Vec3{ .x = 1, .y = 1, .z = 1 }).mult(0.5));
+        return h.n.add(vec3.Vec3{ .x = 1, .y = 1, .z = 1 }).mult(0.5);
     }
 
     const unit = vec3.unit(r.direction);
     const t = 0.5 * (unit.y + 1.0);
-    return rgb.RGB.fromVec3(lerp(vec3.Vec3{
+    return lerp(vec3.Vec3{
         .x = 1.0,
         .y = 1.0,
         .z = 1.0,
@@ -35,24 +40,15 @@ fn ray_color(r: ray.Ray, spheres: []sphere.Sphere) rgb.RGB {
         .x = 0.5,
         .y = 0.7,
         .z = 1.0,
-    }, t));
+    }, t);
 }
 
-pub const log_level: std.log.Level = .info;
-
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
-    // image dimensions
+    const rand = &std.rand.DefaultPrng.init(0).random;
 
     const camera = cam.Camera.init();
 
-    const width = 400;
     const height = @floatToInt(i32, width / cam.aspect_ratio);
-
-
-    // render
-    var pixels: [width][height]rgb.RGB = undefined;
 
     var spheres = [_]sphere.Sphere{
         sphere.Sphere{
@@ -65,17 +61,25 @@ pub fn main() !void {
         },
     };
 
+    var pixels: [width][height]rgb.RGB = undefined;
+
     var j: usize = 0;
     while (j < height) {
-        std.log.info("scanlines remaining: {d}", .{j});
+        std.log.info("render scanlines remaining: {d}", .{j});
         var i: usize = 0;
         while (i < width) {
-            const u = @intToFloat(f32, i) / @intToFloat(f32, width - 1);
-            const v = @intToFloat(f32, j) / @intToFloat(f32, height - 1);
-            const r = camera.createRay(u, v);
-            const pixel_color = ray_color(r, &spheres);
 
-            pixels[i][j] = pixel_color;
+            var sample: usize = 0;
+            var pixelColour = vec3.Vec3{};
+            while (sample < samples) {
+                const u = (@intToFloat(f32, i) + rand.float(f32)) / @intToFloat(f32, width - 1);
+                const v = (@intToFloat(f32, j) + rand.float(f32)) / @intToFloat(f32, height - 1);
+                const r = camera.createRay(u, v);
+
+                pixelColour = pixelColour.add(ray_color(r, &spheres));
+                sample += 1;
+            }
+            pixels[i][j] = rgb.RGB.fromVec3(pixelColour.mult(1.0 / @intToFloat(f32, samples)));
 
             i += 1;
         }
