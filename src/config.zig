60eea1b8414c184c371ee7a884cfb3f6c2a4ea45
Dominic Hamon <dma+github@stripysock.com>
2021-10-15 17:56:09 +0100

    separate config

---
+++ src/config.zig
@@ -0,0 +1,29 @@
+const Config = struct {
+    samples: usize,
+    width: usize,
+    max_depth: u32,
+};
+
+pub fn hi_res() Config {
+    return .{
+        .samples = 1000,
+        .width = 1080,
+        .max_depth = 10,
+    };
+}
+
+pub fn test_render() Config {
+    return .{
+        .samples = 10,
+        .width = 400,
+        .max_depth = 5,
+    };
+}
+
+pub fn low_res() Config {
+    return .{
+        .samples = 100,
+        .width = 640,
+        .max_depth = 5,
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -1,43 +1,32 @@
 const std = @import("std");
 const cam = @import("camera.zig");
-const hit = @import("hit.zig");
+const cfg = @import("config.zig");
 const ppm = @import("ppm.zig");
-const ray = @import("ray.zig");
 const rgb = @import("rgb.zig");
-const sphere = @import("sphere.zig");
-
 const vec3 = @import("vec3.zig");
+const Hit = @import("hit.zig").Hit;
+const Ray = @import("ray.zig").Ray;
+const Sphere = @import("sphere.zig").Sphere;
+
+const Camera = cam.Camera;
 const Vec3 = vec3.Vec3;
 
 pub const log_level: std.log.Level = .info;
 
-// hi-res
-// const SAMPLES = 1000;
-// const WIDTH = 1080;
-// const MAX_DEPTH = 10;
-
-// test render
-// const SAMPLES = 10;
-// const WIDTH = 400;
-// const MAX_DEPTH = 5;
-
-// low res
-const SAMPLES = 100;
-const WIDTH = 640;
-const MAX_DEPTH = 5;
+const config = cfg.hi_res();
 
 fn lerp(a: Vec3, b: Vec3, t: f64) Vec3 {
     return a.mult(f64, 1.0 - t).add(b.mult(f64, t));
 }
 
-fn ray_color(rand: *std.rand.Random, r: ray.Ray, spheres: []sphere.Sphere, depth: u32) Vec3 {
-    if (depth >= MAX_DEPTH) {
+fn ray_color(rand: *std.rand.Random, r: Ray, spheres: []Sphere, depth: u32) Vec3 {
+    if (depth >= config.max_depth) {
         return Vec3.zero();
     }
 
     const closest: f64 = 0.00001;
     var farthest: f64 = 1000.0;
-    var nearestHit: ?hit.Hit = null;
+    var nearestHit: ?Hit = null;
     for (spheres) |sph| {
         const optHit = sph.intersect(r, closest, farthest);
         if (optHit) |h| {
@@ -61,18 +50,15 @@ fn ray_color(rand: *std.rand.Random, r: ray.Ray, spheres: []sphere.Sphere, depth
 }
 
 pub fn main() !void {
+
     const rand = &std.rand.DefaultPrng.init(42).random;
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = &arena.allocator;
+    const camera = Camera.init();
 
-    const camera = cam.Camera.init();
+    const height = @floatToInt(usize, @intToFloat(f64, config.width) / cam.aspect_ratio);
 
-    const height = @floatToInt(i32, WIDTH / cam.aspect_ratio);
-
-    var spheres = [_]sphere.Sphere{
-        sphere.Sphere{
+    var spheres = [_]Sphere{
+        Sphere{
             .center = Vec3.init(0.3, 0.0, -1.0),
             .radius = 0.5,
             .materials = .{ .lambFac = 1.0, .lamb = .{
@@ -84,7 +70,7 @@ pub fn main() !void {
             } },
         },
 
-        sphere.Sphere{
+        Sphere{
             .center = Vec3.init(-0.6, -0.2, -1.2),
             .radius = 0.3,
             .materials = .{
@@ -101,7 +87,7 @@ pub fn main() !void {
             },
         },
 
-        sphere.Sphere{
+        Sphere{
             .center = Vec3.init(-0.15, -0.4, -0.7),
             .radius = 0.1,
             .materials = .{
@@ -118,7 +104,7 @@ pub fn main() !void {
         },
 
         // ground
-        sphere.Sphere{
+        Sphere{
             .center = Vec3.init(0.0, -100.5, -1.0),
             .radius = 100.0,
             .materials = .{ .lambFac = 1.0, .lamb = .{
@@ -127,7 +113,7 @@ pub fn main() !void {
         },
     };
 
-    var pixels: [WIDTH][height]rgb.RGB = undefined;
+    var pixels: [config.width][height]rgb.RGB = undefined;
 
     var j: usize = 0;
     while (j < height) {
@@ -135,18 +121,18 @@ pub fn main() !void {
             std.log.info("rendering scanline {d} / {d}", .{ j, height });
         }
         var i: usize = 0;
-        while (i < WIDTH) {
+        while (i < config.width) {
             var sample: usize = 0;
             var pixelColour = Vec3.zero();
-            while (sample < SAMPLES) {
-                const u = (@intToFloat(f64, i) + rand.float(f64)) / @intToFloat(f64, WIDTH - 1);
+            while (sample < config.samples) {
+                const u = (@intToFloat(f64, i) + rand.float(f64)) / @intToFloat(f64, config.width - 1);
                 const v = (@intToFloat(f64, j) + rand.float(f64)) / @intToFloat(f64, height - 1);
                 const r = camera.createRay(u, v);
 
                 pixelColour = pixelColour.add(ray_color(rand, r, &spheres, 0));
                 sample += 1;
             }
-            pixels[i][j] = rgb.RGB.fromVec3(pixelColour.mult(f64, 1.0 / @intToFloat(f64, SAMPLES)).pow(1.0 / 2.2));
+            pixels[i][j] = rgb.RGB.fromVec3(pixelColour.mult(f64, 1.0 / @intToFloat(f64, config.samples)).pow(1.0 / 2.2));
 
             i += 1;
         }
